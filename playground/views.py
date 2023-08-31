59eb7251b7943742daa11bca2e4dcd342b232760
bzuhal <30375098+bzuhal@users.noreply.github.com>
2023-08-31 08:33:00 +0200

    filter bad words

--- playground/views.py
+++ playground/views.py
@@ -14,14 +14,19 @@ import requests
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
-
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.contrib.auth.hashers import make_password
+import os
+import os.path
 from .forms import *
 from .models import *
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    stories = storyHistory.objects.filter(user_id=request.user.id)
+    return render(request, 'index.html', {'stories': stories})
+
 
 def login(request):
     if request.method == 'POST':
@@ -34,6 +39,7 @@ def login(request):
             return render(request, 'login.html', {'error': 'Username or password is incorrect!'})
     else:
         return render(request, 'login.html')
+
 
 def register(request):
     if request.method == "POST":
@@ -51,12 +57,14 @@ def register(request):
     else:
         return render(request, 'register.html')
 
+
 def logout_user(request):
     if request.user.is_authenticated:
         logout(request)
         return redirect(index)
     else:
         return redirect(index)
+
 
 def profile_user(request):
     check_user_login(request)
@@ -67,18 +75,18 @@ def profile_user(request):
         user = User.objects.get(pk=request.user.id)
         user.first_name = request.POST['first_name']
         user.last_name = request.POST['last_name']
-        
+
         if len(request.POST['password1']) > 0:
             if request.POST['password1'] == request.POST['password2']:
                 user.password = make_password(request.POST['password1'])
             else:
                 return render(request, 'profile.html', {'error': 'Password does not match!'})
-            
+
         user.save()
         return logout_user(request)
-    
+
     else:
-        return render(request, 'profile.html', {'stories' : stories})
+        return render(request, 'profile.html', {'stories': stories})
 
 
 def check_user_login(request):
@@ -100,12 +108,21 @@ def check_user_login(request):
 #         form = SearchStoryForm()
 #         return render(request, 'index.html', {"form": form})
 
+def read_story(request, id):
+    check_user_login(request)
+
+    stories = storyHistory.objects.filter(id=id)
+
+    return render(request, 'read_story.html', {'stories' : stories})
+
+
+
 def search_story(request):
     check_user_login(request)
 
     your_name = ""
     your_friend_name = ""
-    story = ""
+    story_you_want = ""
 
     if request.method == "POST":
         your_name = request.POST.get("your_name")
@@ -113,21 +130,41 @@ def search_story(request):
         story_you_want = request.POST.get("story_you_want")
 
         if your_name and your_friend_name and story_you_want:
-            story = generate_story(your_name, your_friend_name, story_you_want)
-            userobj = User.objects.get(id=1)
-            # saving the story
-            history_entry = storyHistory(
-                user_id=userobj,
-                name=your_name,
-                friend_name=your_friend_name,
-                story_topic=story_you_want,
-                generated_story=story
-            )
-            history_entry.save()
+            search_key_words = (your_name + " " + your_friend_name + " " + story_you_want).split(" ")
+            contain_bad_words = check_contain_bad_word(search_key_words)
+            if contain_bad_words == True:
+                story = "You are not allowed to search for these word"
+                return render(request, 'search.html', {"your_name": your_name, "your_friend_name": your_friend_name, "story_you_want": story_you_want, "story": story})
+            else:
+                story = generate_story(your_name, your_friend_name, story_you_want)
+                userobj = User.objects.get(id=1)
+                # saving the story
+                history_entry = storyHistory(
+                    user_id=userobj,
+                    name=your_name,
+                    friend_name=your_friend_name,
+                    story_topic=story_you_want,
+                    generated_story=story
+                )
+                history_entry.save()
 
-            return render(request, 'search.html', {"your_name": your_name, "your_friend_name": your_friend_name, "story_you_want": story_you_want, "story": story})
+                return render(request, 'search.html', {"your_name": your_name, "your_friend_name": your_friend_name, "story_you_want": story_you_want, "story": story})
+        else:
+            message = "You have to fill all inputs"
+            return render(request, 'search.html', {"your_name": your_name, "your_friend_name": your_friend_name, "story_you_want": story_you_want, "message": message})
 
     return render(request, 'search.html')
+
+def check_contain_bad_word(search_keywords):
+    p = staticfiles_storage.path('bad_words.txt')
+    content = open(p).readlines()[0]
+    bad_words = content.split(",")
+    for keyword in search_keywords:
+        if keyword in bad_words:
+            return True
+
+    return False
+
 
 def generate_story(name, friend, story):
     base_url = "https://vicuna-api.aieng.fim.uni-passau.de/v1"
@@ -138,7 +175,7 @@ def generate_story(name, friend, story):
     data = {
         "model": "gpt-3.5-turbo",
         "messages": [
-            {"role": "system","content": f"Create a child friendly story using protagonist:{name}, friend's name:{friend}, and on the topic:{story}. Do not have violent words in the story and let it have a good moral message."},
+            {"role": "system", "content": f"Create a child friendly story using protagonist:{name}, friend's name:{friend}, and on the topic:{story}. Do not have violent words in the story and let it have a good moral message."},
         ]
     }
 
@@ -155,13 +192,15 @@ def generate_story(name, friend, story):
     else:
         return f"Error: {response.status_code}, Response: {response.text}"
 
+
 def get_story():
     name = "rat"
     friend = "lamp"
     story = "blueberry"
-    
+
     story = generate_story(name, friend, story)
     print("Story:", story)
+
 
 def custom_404(request, exception):
     return render(request, '404.html', status=404)
